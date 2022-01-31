6a2355ca301cba40bd5530fc59f0eeb33c87e886
Jason Skidis <jason@skidis.com>
2022-01-30 23:21:41 -0600

    First full pass of the integrated algorithm. Seems to work in most cases, but found at least one corner case

--- src/main/scala/Wordle.scala
+++ src/main/scala/Wordle.scala
@@ -1,24 +1,51 @@
 package com.skidis.wordle
 
-import BlockColor.{Black, BlockColor, Green}
+import BlockColor.{BlockColor, Green}
 
-import scala.io.Source
+import scala.annotation.tailrec
+import scala.io.{Source, StdIn}
 
 object Wordle extends App {
-  val currentGuess = "TRACE"
+  val winningColorPattern = List(Green, Green, Green, Green, Green)
   val candidateWords = WordReader(Source.fromResource("words.txt"))
+  val initialGuess = "TRACE"
 
-//  val colorPattern = GatherResult(StdIn.readLine, Console.println, InputValidator.apply)
-  val colorPattern = List(Green, Green, Black, Black, Green)
-  println(colorPattern match {
-    case Nil => "Process Aborted By User"
-    case _ => colorPattern.mkString(", ")
+  val result = process(initialGuess, candidateWords)
+
+  println(result match {
+    case None => "Process Aborted By User"
+    case Some(word) => s"Correct Answer: $word"
   })
 
-  val wordPattern: List[(Char, BlockColor)] = currentGuess.toList zip colorPattern
+  @tailrec
+  def process(currentGuess: String, wordSet: Set[String], guessNumber: Int = 1): Option[String] = {
+    println(s"Current Guess: $currentGuess, Guess #:$guessNumber")
 
-//  val matchingDetail = GenerateMatchingDetail(currentGuess, colorPattern)
-  val remainingWords = candidateWords.filter(WordMatchesWordPattern(_, wordPattern))
+    // Generate Color Pattern Based On Input
+    val colorPattern = GatherResult(StdIn.readLine, Console.println, InputValidator.apply)
 
-  remainingWords foreach { println }
+    if (colorPattern.isEmpty) None // User entered an empty string, so abort without finishing
+    else if (colorPattern == winningColorPattern) Option(currentGuess) // color pattern is all green, so word has been guessed
+    else {
+      println(s"Color Pattern: ${colorPattern.mkString(", ")}")
+
+      // Create list of tuple with each letter of the current word and the color for that letter
+      val wordPattern: List[(Char, BlockColor)] = currentGuess.toList zip colorPattern
+
+      // Eliminate words from set that don't fit color pattern for current answer
+      val remainingWords = wordSet.filter(w => w != currentGuess && WordMatchesWordPattern(w, wordPattern))
+
+      // Generate a set with a pair of each word and the number of unique clusters from remaining word set for that word
+      val wordsNumClusters: Set[(String, Int)] = remainingWords.map { word =>
+        (word, GenerateWordClusters(word, remainingWords).size)
+      }
+
+      // Next Guess is based on word with most unique clusters
+      val wordWithMostClusters = wordsNumClusters.reduceLeft { (left, right) => if (left._2 >= right._2) left else right }
+      val nextGuess = wordWithMostClusters._1
+
+      // Start over with next guess
+      process(nextGuess, remainingWords, guessNumber +1)
+    }
+  }
 }
