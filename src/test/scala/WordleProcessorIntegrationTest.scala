41a65947e821a2b7d6753416b2e68099fc254ace
Jason Skidis <jason@skidis.com>
2022-02-07 12:08:27 -0600

    Multi-threaded the integration test Reduced time to run by 85% on a 8-core machine

--- src/test/scala/WordleProcessorIntegrationTest.scala
+++ src/test/scala/WordleProcessorIntegrationTest.scala
@@ -1,5 +1,8 @@
 package com.skidis.wordle
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 import scala.io.Source
 
 object WordleProcessorIntegrationTest extends App {
@@ -7,20 +10,26 @@ object WordleProcessorIntegrationTest extends App {
   val answers = WordReader.read(Source.fromResource("answers.txt"))
 
   val startTimestamp = System.currentTimeMillis()
-  val results: Set[Option[(String, Int)]] = answers.map { answer =>
-    val colorPatternGenerator: ColorPatternGenerator = WordColorPatternGenerator.generateCurryable(answer)
-    WordleProcessor.process(colorPatternGenerator, debugOutput = false)(candidateWords)
-  }
+
+  val results: Set[Option[(String, Int)]] = Await.result(Future.sequence(answers.map(runWordle)), 10.minutes)
+  printResults(results)
 
   val endTimestamp = System.currentTimeMillis()
   println(s"Time Elapsed: ${(endTimestamp - startTimestamp)/1000}")
 
-  val groupedByGuesses: List[(Int, Set[Option[(String, Int)]])] = results.groupBy{
-    case None => -1
-    case Some((_, numGuesses)) => numGuesses
-  }.toList.sortWith(_._1 < _._1)
+  def runWordle(answer: String): Future[Option[(String, Int)]] = Future {
+    val colorPatternGenerator: ColorPatternGenerator = WordColorPatternGenerator.generateCurryable(answer)
+    WordleProcessor.process(colorPatternGenerator, debugOutput = false)(candidateWords)
+  }
 
-  groupedByGuesses.foreach{
-    case (numGuesses, results) => println(s"$numGuesses Guesses: ${results.size}")
+  def printResults(results: Set[Option[(String, Int)]]): Unit = {
+    val groupedByGuesses: List[(Int, Set[Option[(String, Int)]])] = results.groupBy{
+      case None => -1
+      case Some((_, numGuesses)) => numGuesses
+    }.toList.sortWith(_._1 < _._1)
+
+    groupedByGuesses.foreach{
+      case (numGuesses, results) => println(s"$numGuesses Guesses: ${results.size}")
+    }
   }
 }
