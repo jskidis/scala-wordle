7f15bdf0e8eafb913eb9f2663bbf3813e1bd8188
Jason Skidis <jason@skidis.com>
2022-02-15 06:49:12 -0600

    Replaced recursion with map function

--- src/main/scala/InputToColorsConversion.scala
+++ src/main/scala/InputToColorsConversion.scala
@@ -2,19 +2,14 @@ package com.skidis.wordle
 
 import BlockColor.{Black, BlockColor, Green, Yellow}
 
-import scala.annotation.tailrec
-
 object InputToColorsConversion {
-  @tailrec
-  def convert(input: String, colorPattern: List[BlockColor] = Nil): List[BlockColor] = {
-    if (input.isEmpty) colorPattern
-    else {
-      val value = input.head.toUpper match {
+  def convert(input: String): List[BlockColor] = {
+    input.map { ch =>
+      ch.toUpper match {
         case `greenChar` => Green
         case `yellowChar` => Yellow
         case _ => Black
       }
-      convert(input.tail, colorPattern :+ value)
-    }
+    }.toList
   }
 }
