969eb4945691d95d7d1fee4b51e8845f74bbb625
Jason Skidis <jason@skidis.com>
2022-02-16 15:43:41 -0600

    Added even more influence of word frequency compared to number of clusters when choosing next guess

--- src/main/scala/ClusterWithFrequencyStrategy.scala
+++ src/main/scala/ClusterWithFrequencyStrategy.scala
@@ -26,7 +26,7 @@ object ClusterWithFrequencyStrategy extends SolveStrategy {
 
     def sortWordCluster(wc1: WordClusterCount, wc2: WordClusterCount): Boolean = (wc1.word, wc2.word) match {
       case (wwf1:WordleWordFrequencies, wwf2:WordleWordFrequencies) =>
-        wc1.clusterCount + Math.log10(wwf1.frequency)*1 > wc2.clusterCount + Math.log10(wwf2.frequency)*1
+        wc1.clusterCount + Math.log10(wwf1.frequency)*2 > wc2.clusterCount + Math.log10(wwf2.frequency)*2
       case (ww1: WordleWord, ww2: WordleWord) =>
         if(wc1.clusterCount != wc2.clusterCount) wc1.clusterCount > wc2.clusterCount else ww1 > ww2
     }
