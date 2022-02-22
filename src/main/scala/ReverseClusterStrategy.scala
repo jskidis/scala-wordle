3140c0ef9e46b3c9db8c4587419a4389151e2542
Jason Skidis <jason@skidis.com>
2022-02-22 12:59:24 -0600

    Minor tweak to the Reverse (trying to lose) strategy

--- src/main/scala/ReverseClusterStrategy.scala
+++ src/main/scala/ReverseClusterStrategy.scala
@@ -1,7 +1,14 @@
 package com.skidis.wordle
 
 trait ReverseClusterStrategy extends ClusterStrategy {
-  override def sortWordCluster(wc1: WordClusterCount, wc2: WordClusterCount): Boolean = !super.sortWordCluster(wc1, wc2)
+  override def sortWordCluster(wc1: WordClusterCount, wc2: WordClusterCount): Boolean = {
+    if(wc1.clusterCount != wc2.clusterCount) wc1.clusterCount < wc2.clusterCount else wc1.word < wc2.word
+  }
+
+  override def generateNextGuess(remainingWords: WordSet): (String, String) = {
+    val (nextGuess, info) = super.generateNextGuess(remainingWords)
+    (nextGuess, info.replace("Most", "Fewest"))
+  }
 }
 
 object ReverseClusterStrategy extends ReverseClusterStrategy
