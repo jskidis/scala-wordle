adb794b0176d0116e1bec6cdf71eb3410720a8a7
Jason Skidis <jason@skidis.com>
2022-02-26 16:11:26 -0600

    Started an integration test for the simulators, didn't finish

--- src/test/scala/simulation/NerdleSimulation.scala
+++ src/test/scala/simulation/NerdleSimulation.scala
@@ -10,19 +10,25 @@ import scala.concurrent.{Await, Future}
 import scala.util.Random
 
 object NerdleSimulation extends App with SimResultsPrinter {
+  val numSimulations = if (args.length > 1) args(1).toInt else Int.MaxValue
+
   val startEquation = "58-42=16"
   val equations = NerdleGuessableGenerator.generatateEquations()
-  val answers = Random.shuffle(equations)
+  val answers = Random.shuffle(equations).take(numSimulations)
 
-  val startTimestamp = System.currentTimeMillis()
+  runSimulation()
 
-  val results: Vector[Seq[(String, WordHints)]] = Await.result(
-    Future.sequence(answers.map(answer => runWordle(answer.phrase))), 1.hour
-  ).toVector
-  printResults(results)
+  def runSimulation(): Unit = {
+    val startTimestamp = System.currentTimeMillis()
 
-  val endTimestamp = System.currentTimeMillis()
-  println(s"Time Elapsed: ${(endTimestamp - startTimestamp) / 1000}")
+    val results: Vector[Seq[(String, WordHints)]] = Await.result(
+      Future.sequence(answers.map(answer => runWordle(answer.phrase))), 1.hour
+    ).toVector
+    printResults(results)
+
+    val endTimestamp = System.currentTimeMillis()
+    println(s"Time Elapsed: ${(endTimestamp - startTimestamp) / 1000}")
+  }
 
   def runWordle(answer: String): Future[Seq[(String, WordHints)]] = Future {
     val processor = new SimulationProcessor(answer, NerdleHintProps) with ClusterStrategy
--- src/test/scala/simulation/WordleSimulation.scala
+++ src/test/scala/simulation/WordleSimulation.scala
@@ -10,35 +10,49 @@ import scala.concurrent.{Await, Future}
 import scala.io.Source
 
 object WordleSimulation extends App with WordReader with SimResultsPrinter {
-  case class Parameters(startWord: String, wordSet: WordSet, processorFactory: String => SimulationProcessor)
+  case class Parameters(startWord: String, wordSet: WordSet,
+    processorFactory: String => SimulationProcessor, maxSimulations: Int = Int.MaxValue)
 
-  val parameters = (if (args.length > 0) args(0) else "") match {
-    case s if s == "answer-only" => Parameters("SLATE",
-      readWords(Source.fromResource("answers.txt")),
-      (answer: String) => new SimulationProcessor(answer, WordleHintProps) with ClusterAndFreqStrategy
-    )
-    case s if s == "reverse" => Parameters("JAZZY",
-      readWordFrequencies(Source.fromResource("words-filtered-by-frequency.txt")),
-      (answer: String) => new SimulationProcessor(answer, WordleHintProps) with ReverseClusterStrategy
-    )
-    case _ => Parameters("SLATE",
-      readWordFrequencies(Source.fromResource("word-frequency-filtered.txt")),
-      (answer: String) => new SimulationProcessor(answer, WordleHintProps) with ClusterAndFreqStrategy
-    )
+  val answerOnlyParams = Parameters("SLATE",
+    readWords(Source.fromResource("answers.txt")),
+    (answer: String) => new SimulationProcessor(answer, WordleHintProps) with ClusterAndFreqStrategy
+  )
+
+  val reverseStratParam = Parameters("JAZZY",
+    readWordFrequencies(Source.fromResource("words-filtered-by-frequency.txt")),
+    (answer: String) => new SimulationProcessor(answer, WordleHintProps) with ReverseClusterStrategy
+  )
+
+  val standardParams = Parameters("SLATE",
+    readWordFrequencies(Source.fromResource("word-frequency-filtered.txt")),
+    (answer: String) => new SimulationProcessor(answer, WordleHintProps) with ClusterAndFreqStrategy
+  )
+
+  val numSimulations = if (args.length > 1) args(1).toInt else Int.MaxValue
+
+  val parameters = ((if (args.length > 0) args(0) else "") match {
+    case s if s == "answer-only" => answerOnlyParams
+    case s if s == "reverse" => reverseStratParam
+    case _ => standardParams
+  }).copy(maxSimulations = numSimulations)
+
+
+  runSimulation(parameters)
+
+  def runSimulation(parameters: Parameters = standardParams): Unit = {
+    val answers = readWords(Source.fromResource("answers.txt")).take(parameters.maxSimulations)
+    val startTimestamp = System.currentTimeMillis()
+
+    val results: Vector[Seq[(String, WordHints)]] = Await.result(
+      Future.sequence(answers.map(answer => runWordle(answer.phrase, parameters))), 1.hour
+    ).toVector
+    printResults(results)
+
+    val endTimestamp = System.currentTimeMillis()
+    println(s"Time Elapsed: ${(endTimestamp - startTimestamp) / 1000}")
   }
 
-  val answers = readWords(Source.fromResource("answers.txt"))
-  val startTimestamp = System.currentTimeMillis()
-
-  val results: Vector[Seq[(String, WordHints)]] = Await.result(
-    Future.sequence(answers.map(answer => runWordle(answer.phrase))), 1.hour
-  ).toVector
-  printResults(results)
-
-  val endTimestamp = System.currentTimeMillis()
-  println(s"Time Elapsed: ${(endTimestamp - startTimestamp) / 1000}")
-
-  def runWordle(answer: String): Future[Seq[(String, WordHints)]] = Future {
+  def runWordle(answer: String, parameters: Parameters): Future[Seq[(String, WordHints)]] = Future {
     val processor = parameters.processorFactory(answer)
     val result = processor.process(parameters.wordSet, parameters.startWord)
     //    println("Processed")
