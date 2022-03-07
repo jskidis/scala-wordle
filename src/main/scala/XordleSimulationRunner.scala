a2f6c7ba9adb3687589bf4535be41ad2b99f7fb5
Jason Skidis <jason@skidis.com>
2022-03-07 13:34:20 -0600

    Enabled simulator to be able to run with supplied starting guesses

--- src/main/scala/XordleSimulationRunner.scala
+++ src/main/scala/XordleSimulationRunner.scala
@@ -5,15 +5,19 @@ import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 
 trait XordleSimulationRunner extends XordleRunner {
-  def runSimulation(): Unit = {
+  def runSimulation(): Unit = runSimulation(None)
+
+  def runSimulation(suppliedStartGuess: Option[String]): Unit = {
     val startTimestamp = System.currentTimeMillis()
 
     val answers = answerSet.toSeq.map(_.phrase)
+    val theStartingGuess = suppliedStartGuess.getOrElse(startGuess)
     val processor = createSimulationProcessor()
 
+    println(s"Starting Word: $theStartingGuess")
     val results = for {
       result <- Await.result(Future.sequence(
-        answers.map{ answer => runWordle(processor, answer) }
+        answers.map{ answer => Future(runWordle(processor, answer, theStartingGuess)) }
       ), 1.hour)
     } yield result
     printResults(results)
@@ -22,12 +26,11 @@ trait XordleSimulationRunner extends XordleRunner {
     println(s"Time Elapsed: ${(endTimestamp - startTimestamp) / 1000}")
   }
 
-  def runWordle(processor: SimulationProcessor, answer: String): Future[Int] = Future {
-    processor.process(guessSet, startGuess, answer) match {
+  def runWordle(processor: SimulationProcessor, answer: String, theStartingGuess: String): Int = {
+    processor.process(guessSet, theStartingGuess, answer) match {
       case Left(_) => -1
-      case Right(result) =>
-        //    println(s"Processes: $answer - ${result.size} Guesses")
-        result.size
+      case Right(result) => result.size
+      //    println(s"Processes: $answer - ${result.size} Guesses")
     }
   }
 
