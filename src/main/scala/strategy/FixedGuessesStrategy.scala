230ea30b7e438bcb0b1275a343daba238b47947c
Jason Skidis <jason@skidis.com>
2022-03-15 15:01:43 -0500

    Changed processor to get first guess from strategy object instead of needing to be supplied to the processor The strategy object is still able to provide a fixed first (and even subsequent) guesses for performance reasons, but that is now hidden from the processor. Also added a wordle strategy that scores a word by on character frequency in the word set (with a bonus for being at the right position). It's not as optimal as the cluster and word frequency strategy, but it's close Some minor refactoring of a few other things while I was in there

---
+++ src/main/scala/strategy/FixedGuessesStrategy.scala
@@ -0,0 +1,12 @@
+package com.skidis.wordle
+package strategy
+
+trait FixedGuessesStrategy extends SolveStrategy {
+  def fixedGuesses: Seq[String]
+
+  abstract override def generateNextGuesses(remainingWords: WordSet, previousGuesses: Seq[String], numToReturn: Int)
+  : Seq[String] = {
+    if(fixedGuesses.size > previousGuesses.size) Vector(fixedGuesses(previousGuesses.size))
+    else super.generateNextGuesses(remainingWords, previousGuesses, numToReturn)
+  }
+}
