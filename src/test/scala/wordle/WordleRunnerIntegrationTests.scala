4e195c56b95fc7f51c722f3e0280a1d3eece5d2d
jskidis <bi8b0AT1718>
2022-03-26 14:53:44 -0500

    Fixed testing mechanism from last commit

--- src/test/scala/wordle/WordleRunnerIntegrationTests.scala
+++ src/test/scala/wordle/WordleRunnerIntegrationTests.scala
@@ -6,44 +6,47 @@ import TestFixtures.TWord
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 
-import scala.collection.mutable.ListBuffer
-
 class WordleRunnerIntegrationTests extends AnyFunSuite with Matchers {
   val slimAnswerSet: Set[TWord] = Set("SLATE", "TRACE", "JAZZY").map(TWord)
 
-  def didAnySimulationsReturnErrors(linesWritten: ListBuffer[String]): Boolean = {
-    linesWritten.exists(l => l.contains("-1 Guesses"))
+  def didAnySimulationsReturnErrors(results: (Seq[Int], Long)): Boolean = {
+    val guesses = results._1
+    guesses.contains(-1)
   }
 
   test("Run Wordle Simulation Apps", IntegrationTest) {
-    val standardFixture = new WordleSimulationStandardRunner with WriterToBuffer {
+    val standardFixture = new WordleSimulationStandardRunner {
       override lazy val answerSet: WordSet = slimAnswerSet
     }
-    standardFixture.main(Array())
-    didAnySimulationsReturnErrors(standardFixture.linesWritten) must not be true
+    didAnySimulationsReturnErrors(standardFixture.runSimulation()) must not be true
 
-    val answerOnlyFixture = new WordleSimulationAnswerOnlyRunner with WriterToBuffer {
+    val answerOnlyFixture = new WordleSimulationAnswerOnlyRunner {
       override lazy val answerSet: WordSet = slimAnswerSet
     }
-    answerOnlyFixture.main(Array())
-    didAnySimulationsReturnErrors(answerOnlyFixture.linesWritten) must not be true
+    didAnySimulationsReturnErrors(answerOnlyFixture.runSimulation()) must not be true
 
-    val charFreqFixture = new WordleSimulationCharFreqRunner with WriterToBuffer {
+    val charFreqFixture = new WordleSimulationCharFreqRunner {
       override lazy val answerSet: WordSet = slimAnswerSet
     }
-    charFreqFixture.main(Array())
-    didAnySimulationsReturnErrors(charFreqFixture.linesWritten) must not be true
+    didAnySimulationsReturnErrors(charFreqFixture.runSimulation()) must not be true
 
-    val wordFreqFixture = new WordleSimulationWordFreqRunner with WriterToBuffer {
+    val wordFreqFixture = new WordleSimulationWordFreqRunner {
       override lazy val answerSet: WordSet = slimAnswerSet
     }
-    wordFreqFixture.main(Array())
-    didAnySimulationsReturnErrors(wordFreqFixture.linesWritten) must not be true
+    didAnySimulationsReturnErrors(wordFreqFixture.runSimulation()) must not be true
 
-    val reverseFixture = new WordleSimulationReverseRunner with WriterToBuffer {
+    val reverseFixture = new WordleSimulationReverseRunner {
       override lazy val answerSet: WordSet = slimAnswerSet
     }
-    reverseFixture.main(Array())
-    didAnySimulationsReturnErrors(reverseFixture.linesWritten) must not be true
+    didAnySimulationsReturnErrors(reverseFixture.runSimulation()) must not be true
   }
+
+/*
+  test("Run Wordle Interactive Runners", IntegrationTest) {
+    val standardFixture = new WordleInteractiveStandardRunner {
+    }
+    standardFixture.run()
+
+  }
+*/
 }
