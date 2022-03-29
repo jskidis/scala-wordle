a6d37ffc02a47cc643264e622a97ee0279c94344
jskidis <bi8b0AT1718>
2022-03-29 17:17:53 -0500

    Removed the unnecessary trait definitions for cluster caching mix-ins as the CachingWordHintsGenerator is mixed-in directly as needed

--- src/main/scala/strategy/ReverseScoringStrategy.scala
+++ src/main/scala/strategy/ReverseScoringStrategy.scala
@@ -1,12 +1,8 @@
 package com.skidis.wordle
 package strategy
 
-import hintgen.CachingWordHintsGenerator
-
 trait ReverseScoringStrategy extends WordScoringStrategy {
   override def sortWordScores(wordScores: Vector[WordScore]): Vector[WordScore] = wordScores.sortBy(_.score)
 }
 
 trait ReverseClusterStrategy extends ClusterStrategy with ReverseScoringStrategy
-
-trait ReverseClusterStrategyCaching extends ReverseClusterStrategy with CachingWordHintsGenerator
